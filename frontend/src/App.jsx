a0f72c48c3498357b0ae93a8a1370b226e992194
Nick Saveliev <kolya59264@gmail.com>
2024-10-16 13:50:54 +0300

    Create a header and styles for it. Add images and icons.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
 
-const App = () => {
+// deleting return fixes header
 
+const App = () => {
+    return (
+        <main>
+            <h1>Choose your hero!</h1>
+        </main>
+    )
 }
 
 export default App
