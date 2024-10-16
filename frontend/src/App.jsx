a473d282aae915ea2b4c015ca35d1d8791141a19
Nick Saveliev <kolya59264@gmail.com>
2024-10-16 23:11:14 +0300

    Fix containers alignment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,7 @@ import { useState } from 'react'
 // deleting return fixes header
 
 const App = () => {
-    return (
-        <main>
-            <h1>Choose your hero!</h1>
-        </main>
-    )
+
 }
 
 export default App
