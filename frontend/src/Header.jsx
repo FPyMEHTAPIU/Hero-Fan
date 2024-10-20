a5ada8f984797d9883b3eaf74007f4bee34f1a34
Nick Saveliev <kolya59264@gmail.com>
2024-10-20 21:22:33 +0300

    Fix popup overlay

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import './Header.css';
 
 const Header = () => {
     const [isWindowShown, setIsWindowShown] = useState(false);
     const [windowType, setWindowType] = useState('Log in');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const changeWindowType = () => {
         if (windowType === 'Log in') {
@@ -34,40 +37,52 @@ const Header = () => {
                 <img src="../includes/User%20Default_Cover.svg" className="avatar" alt="User Avatar" />
                 <p className="username">Log in</p>
             </button>
-            {isWindowShown && <Popup winType={windowType} onChange={changeWindowType} onClose={closePopup} />}
+            {isWindowShown && <Popup
+                winType={windowType} onChange={changeWindowType} onClose={closePopup}
+                password={password} setPassword={setPassword}
+                confirmPassword={confirmPassword} setConfirmPassword={setConfirmPassword}
+            />}
         </header>
     );
 };
 
-const Popup = ({ winType, onChange, onClose }) => {
-    return (
-        <div className="popup-overlay">
-            <div className="popup">
-                <h2>{winType}</h2>
-                <button className="cross-button" onClick={onClose}>
-                    <img src="../includes/cross.svg" alt="Close" />
-                </button>
-                <div className="popup-content">
-                    <p className="input-name">Login</p>
-                    <input name="login input"/>
-                    <p className="input-name">Password</p>
-                    <input name="password input" type="password"/>
-                    {winType === 'Register' && (
-                        <>
-                            <p className="input-name">Confirm Password</p>
-                            <input
-                                name="confirmPassword"
-                                type="password"
-                            />
-                        </>
-                    )}
-                </div>
-                <button className="switch" onClick={onChange}>
-                    <p>Don't have an account? Create the new one!</p>
-                </button>
-                <button className="confirm-button">{winType}</button>
+const Popup = ({ winType, onChange, onClose,
+               password, setPassword, confirmPassword, setConfirmPassword }) => {
+    return ( createPortal(
+    <div className="popup-overlay">
+        <div className="popup">
+            <h2>{winType}</h2>
+            <button className="cross-button" onClick={onClose}>
+                <img src="../includes/cross.svg" alt="Close"/>
+            </button>
+            <div className="popup-content">
+                <p className="input-name">Login</p>
+                <input name="login input"/>
+                <p className="input-name">Password</p>
+                <input
+                    name="password input"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                {winType === 'Register' && (
+                    <>
+                        <p className="input-name">Confirm Password</p>
+                        <input
+                            name="confirmPassword"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                    </>
+                )}
             </div>
+            <button className="switch" onClick={onChange}>
+                <p>Don't have an account? Create the new one!</p>
+            </button>
+            <button className="confirm-button">{winType}</button>
         </div>
+    </div>, document.body)
     );
 };
 
